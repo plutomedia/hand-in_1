048f630f64f332021735105f6038446af319d190
espen brune <ice@mailfence.com>
2016-01-10 16:26:51 +0100

    minor change1

---
+++ js/input.js
@@ -0,0 +1,18 @@
+/*!
+ * Form input treatment. Just for the sake of it
+ */
+ 
+ 
+ 
+ 
+ /**
+ function validateForm() {
+    var x = document.forms["inputform"]["navn"].value;
+	alert(x);
+    if (x == null || x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+	alert("skjemaet er fylt ut");
+}
+*/
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Hand-in_1
+This is the repo for 1st hand-in in dat 219, UiA, spring 2016.
+Author: Espen Brune, ice@mailfence.com
